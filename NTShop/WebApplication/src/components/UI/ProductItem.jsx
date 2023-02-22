96a88783063e0d63dce1ed03dc6bbbc07fb1b838
nntram <tramb9531@gmail.com>
2023-02-22 21:16:57 +0700

    edit product card

--- NTShop/WebApplication/src/components/UI/ProductItem.jsx
+++ NTShop/WebApplication/src/components/UI/ProductItem.jsx
@@ -7,8 +7,8 @@ import { Link } from 'react-router-dom'
 const ProductItem = ({item}) => {
     
     return (
-        <Col lg='3' md='4' className='mb-2'>
-            <div className='product__item'>
+        <Col lg='3' md='4' className='my-2'>
+            <div className='product__item border'>
                 <div className="product__img">
                     <motion.img whileHover={{scale:0.9}} alt=""
                     src={require("../../assets/image_data/products/" + item.productimages)} />
@@ -16,17 +16,34 @@ const ProductItem = ({item}) => {
                 
                 <div className="p-2 product__info">
                 <h3 className='product__name'>
-                    <Link to={`/shop/${item.productid}`}>{item.productname}</Link>
+                    <Link to={`/shop/${item.productid}`}
+                     data-toggle="tooltip" title={item.productname}>
+                        {item.productname}
+                    </Link>
                 </h3>
                 <span>{item.categoryname}</span>
                 </div>
-                <div className="product__card-bottom d-flex align-items-center 
-                                justify-content-between p-2">
+                <div className="product__card-bottom d-flex 
+                        align-items-center justify-content-center gap-3 p-2">
+                    {
+                        (item.productprice !== item.productsaleprice) ? 
+                            <del className="">
+                                {item.productprice.toLocaleString()} đ
+                             </del> : ""
+                        
+
+                    }
                     <span className="price">
-                       {item.productprice} VNĐ
-                    </span>
+                       {item.productsaleprice.toLocaleString()} đ
+                    </span>                  
+                </div>
+                <div className="product__card-bottom d-flex 
+                        align-items-center justify-content-center gap-3 p-2">                    
                     <motion.span whileHover={{scale:1.2}} >
-                        <i className="ri-add-line"></i>
+                        <i className="ri-shopping-cart-2-line"></i>
+                    </motion.span>
+                    <motion.span whileHover={{scale:1.2}} >
+                        <i className="ri-heart-line"></i>
                     </motion.span>
                 </div>
             </div>
